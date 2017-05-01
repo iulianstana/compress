a514bd497e6dc4d8f21f5feaea1ee7e9a7d04db5
Iulian Stana <julian.stana@gmail.com>
2017-05-01 18:03:16 +0300

    Manage the constant value from tests

--- compression/compression_test.go
+++ compression/compression_test.go
@@ -27,30 +27,25 @@ func (suite *DriverSuite) SetupSuite() {
 
 func (suite *DriverSuite) TestUpdateValue() {
 	t := suite.T()
-	attribute := "compression"
-	entry := bson.M{
-		"_id": attribute,
-		"counter": 3,
-		"Romania": 0,
-		"Bucharest": 1,
-		"Azimut": 2,
 
-	}
+	attribute, entry := generateEntry()
 	err := suite.driver.Connection.Insert(&entry)
 	assert.Nil(t, err, "Mongo could not Insert entry")
 
+	entryValue := 0
 	// Value is already in ValueToKey structure
-	err = suite.driver.UpdateValue(attribute, 0)
+	err = suite.driver.UpdateValue(attribute, entryValue)
 	assert.Nil(t, err, "Value was not fount in ValueToKey Structure")
 
 	// Reload structure and test if my value appeared
-	delete(suite.driver.ValueToKey[attribute], 0)
-	err = suite.driver.UpdateValue(attribute, 0)
+	delete(suite.driver.ValueToKey[attribute], entryValue)
+	err = suite.driver.UpdateValue(attribute, entryValue)
 	assert.Nil(t, err, "Value was not fount in ValueToKey Structure")
 
 	// Value is not found in ValueToKey structure return err
-	expectReturnError := errors.New(fmt.Sprintf("Value 10 does not exist for attribute %s", attribute))
-	err = suite.driver.UpdateValue(attribute, 10)
+	unknownValue := entry["counter"].(int) + 10
+	expectReturnError := errors.New(fmt.Sprintf("Value %d does not exist for attribute %s", unknownValue, attribute))
+	err = suite.driver.UpdateValue(attribute, unknownValue)
 	assert.Equal(t, err, expectReturnError, "Error message was not returned")
 }
 
@@ -58,6 +53,23 @@ func (suite *DriverSuite) TestUpdateValue() {
 func (suite *DriverSuite) TestLoadAttribute() {
 	t := suite.T()
 
+	attribute, entry := generateEntry()
+	err := suite.driver.Connection.Insert(&entry)
+	assert.Nil(t, err, "Mongo could not Insert entry")
+
+	key, value := "Romania", 0 // use random element from entry
+	// Load attribute in our driver
+	err = suite.driver.LoadAttribute(attribute)
+	err = suite.driver.LoadAttribute(attribute)
+
+	assert.Nil(t, err, "Mongo could not find any result")
+	assert.Equal(t, len(suite.driver.KeyToValue[attribute]), len(entry) - 2, "There are different number of elements")
+	assert.Equal(t, len(suite.driver.ValueToKey[attribute]), len(entry) - 2, "There are different number of elements")
+	assert.Equal(t,suite.driver.ValueToKey[attribute][value], key, "Value number has other key")
+	assert.Equal(t,suite.driver.KeyToValue[attribute][key], value, "Key element has other value")
+}
+
+func generateEntry() (string, bson.M) {
 	attribute := "compression"
 	entry := bson.M{
 		"_id": attribute,
@@ -67,21 +79,8 @@ func (suite *DriverSuite) TestLoadAttribute() {
 		"Azimut": 2,
 
 	}
-	err := suite.driver.Connection.Insert(&entry)
-	assert.Nil(t, err, "Mongo could not Insert entry")
-
-
-	// Load attribute in our driver
-	err = suite.driver.LoadAttribute(attribute)
-	err = suite.driver.LoadAttribute(attribute)
-
-	assert.Nil(t, err, "Mongo could not find any result")
-	assert.Equal(t, len(suite.driver.KeyToValue[attribute]), len(entry) - 2, "There are different number of elements")
-	assert.Equal(t, len(suite.driver.ValueToKey[attribute]), len(entry) - 2, "There are different number of elements")
-	assert.Equal(t,suite.driver.ValueToKey[attribute][0], "Romania", "Value number has other key")
-	assert.Equal(t,suite.driver.KeyToValue[attribute]["Romania"], 0, "Key element has other value")
+	return attribute, entry
 }
-
 func (suite *DriverSuite) TearDownTest() {
 	suite.driver.Connection.DropCollection()
 }
